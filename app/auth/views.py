726aed49c58a72569e4aee6a8a125c6f58f00509
qizheng365 <qzqz365@126.com>
2017-12-28 23:35:23 +0800

    changed at home

--- app/auth/views.py
+++ app/auth/views.py
@@ -53,17 +53,17 @@ def confirm(token):
     return redirect(url_for("main.index"))
 
 
-@auth.before_app_request
-def before_request():
-    if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
-        return redirect(url_for('auth.uncondirmed'))
-
-
-@auth.route("/unconfirmed")
-def uncondirmed():
-    if current_user.is_anonymous or current_user.confirmed:
-        return redirect(url_for('main.index'))
-    return render_template("auth/unconfirmed.html")
+# @auth.before_app_request
+# def before_request():
+#     if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+#         return redirect(url_for('auth.unconfirmed'))
+#
+#
+# @auth.route("/unconfirmed")
+# def unconfirmed():
+#     if current_user.is_anonymous or current_user.confirmed:
+#         return redirect(url_for('main.index'))
+#     return render_template("auth/unconfirmed.html")
 
 
 @auth.route("/confirm")
@@ -73,3 +73,4 @@ def resend_confirmation():
     send_email(current_user.email,'Confirm Your Account','auth/email/confirm', user=current_user, token=token)
     flash("A new confirmation email has been sent to your by email.")
     return redirect(url_for("main.index"))
+
--- app/email.py
+++ app/email.py
@@ -1,9 +1,16 @@
 from . import mail
 from flask_mail import Message
 from flask import render_template,current_app
+from threading import Thread
+from manage import app
 
 def send_email(to,subject,template,**kwargs):
 	msg = Message(subject, sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
 	msg.body = render_template(template+'.html', **kwargs)
 	msg.html = render_template(template+'.html', **kwargs)
-	mail.send(msg)
+	thread = Thread(target=send_email_thread, args=[app,msg])
+	thread.start()
+
+def send_email_thread(app,msg):
+	with app.app_context():
+		mail.send(msg)
